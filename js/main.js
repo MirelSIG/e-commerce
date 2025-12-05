ed39a554de37199533e63ea4b49582ae12e90686
LuisAlfonso107 <luisalfonsogarcialagos9@gmail.com>
2025-12-05 14:25:28 +0100

    proceso de s

--- js/main.js
+++ js/main.js
@@ -5,16 +5,13 @@ import { cart } from "../components/cart/cart.js"
 import { productsController } from "./products.js"
 import { cartController } from "../components/cart/controller.js"
 
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
 
 
-products.getData()
+
 products.render()
 
-=======
-await productsController.getData()
->>>>>>> 4f24de3c639cf3e139498bbc57af41b5c52fef10
+ productsController.getData()
 header.init()
 navbar.render()
 footer.render()
@@ -46,10 +43,7 @@ btnCart.addEventListener("click", function(){
     cart.toogle()
 })
 
-<<<<<<< HEAD
-=======
 cart.addItem(13)
 cart.addItem(14)
 console.log(cartController.getData());
->>>>>>> 4f24de3c639cf3e139498bbc57af41b5c52fef10
 
