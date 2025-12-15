343e388a59fa7156c5e945610642fcbf14f211d6
LuisAlfonso107 <luisalfonsogarcialagos9@gmail.com>
2025-12-15 10:04:55 +0100

    pagina de detalle1

--- js/productoDetalle.js
+++ js/productoDetalle.js
@@ -1,39 +1,30 @@
-
-import { productsController } from "./products.js"
-import { detalleTemplate  } from "../components/paginaDetalle.template.js"
-import { cart } from "../components/cart/cart.js"
-
-
 export const registro = {
+  container: document.getElementById("product-detail"),
 
-   const container = document.getElementById("product-detail")
+  getProductIdFromURL() {
+    const params = new URLSearchParams(window.location.search)
+    return Number(params.get("id"))
+  },
 
-const getProductIdFromURL = () => {
-  const params = new URLSearchParams(window.location.search)
-  return Number(params.get("id"))
-},
+  async renderProductDetail() {
+    await productsController.getData()
 
-const renderProductDetail = async () => {
-  await productsController.getData()
+    const id = this.getProductIdFromURL()
+    const result = productsController.getById(id)
 
-  const id = getProductIdFromURL()
-  const result = productsController.getById(id)
+    if (!result.status) {
+      this.container.innerHTML = `<p>${result.mensaje}</p>`
+      return
+    }
 
-  if (!result.status) {
-    container.innerHTML = `<p>${result.mensaje}</p>`
-    return
-  }
+    const product = result.data[0]
+    this.container.innerHTML = detalleTemplate(product)
 
-  const product = result.data[0]
-  container.innerHTML = detalleTemplate(product)
-
-  document
-    .querySelector(".btn-add")
-    .addEventListener("click", () => {
+    document.querySelector(".btn-add").addEventListener("click", () => {
       cart.addItem(product.id)
     })
-},
-
-renderProductDetail()
-
+  }
 }
+
+// ejecutar
+registro.renderProductDetail()
